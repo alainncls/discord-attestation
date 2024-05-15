2d009615e7820f8857667f194e8a39659d1724d1
Alain Nicolas <alain.nicolas.perso@gmail.com>
2024-05-15 17:57:52 +0200

    Redirect URL as an env. variable

--- src/components/LoginWithDiscord.tsx
+++ src/components/LoginWithDiscord.tsx
@@ -2,7 +2,7 @@ import {FaDiscord} from "react-icons/fa";
 import './LoginWithDiscord.css';
 
 const CLIENT_ID = import.meta.env.VITE_DISCORD_CLIENT_ID;
-const REDIRECT_URI = 'http://localhost:5173';
+const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URL;
 const SCOPE = 'identify guilds';
 const DISCORD_OAUTH_URL = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=${SCOPE}`;
 

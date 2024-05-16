31264e574875d2b6ac903d91012f09c268d68b8a
Alain Nicolas <alain.nicolas@consensys.net>
2024-05-16 11:48:45 +0200

    Fix the dev env detection

--- functions/api.ts
+++ functions/api.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const DISCORD_CLIENT_ID = process.env.VITE_DISCORD_CLIENT_ID;
-const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET;
-const DEFAULT_REDIRECT_URL = 'http://localhost:5173';
+const {VITE_DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, VITE_REDIRECT_URL} = process.env;
+const DEV_REDIRECT_URL = 'http://localhost:5173';
 
 export async function handler(event: {
-    queryStringParameters: { code: string; method: string; isDev: boolean };
+    queryStringParameters: { code: string; method: string; isDev: string };
     body: string;
     httpMethod: string;
 }) {
@@ -25,9 +24,10 @@ export async function handler(event: {
         };
     }
 
-    if (!DISCORD_CLIENT_ID) {
+    if (!VITE_DISCORD_CLIENT_ID) {
         return {
             statusCode: 500,
+            headers,
             body: JSON.stringify({
                 message: "Discord client ID not set",
             }),
@@ -37,21 +37,34 @@ export async function handler(event: {
     if (!DISCORD_CLIENT_SECRET) {
         return {
             statusCode: 500,
+            headers,
             body: JSON.stringify({
                 message: "Discord client secret not set",
             }),
         };
     }
 
+    if (!VITE_REDIRECT_URL) {
+        return {
+            statusCode: 500,
+            headers,
+            body: JSON.stringify({
+                message: "Redirect URL not set",
+            }),
+        };
+    }
+
+
     const {code, isDev} = event.queryStringParameters;
-    const redirectUri = process.env.VITE_REDIRECT_URL ?? DEFAULT_REDIRECT_URL;
+    const isDevBoolean = isDev === 'true';
+    console.log('redirect_uri', isDevBoolean ? DEV_REDIRECT_URL : VITE_REDIRECT_URL)
 
     const params = new URLSearchParams();
-    params.append('client_id', DISCORD_CLIENT_ID);
+    params.append('client_id', VITE_DISCORD_CLIENT_ID);
     params.append('client_secret', DISCORD_CLIENT_SECRET);
     params.append('grant_type', 'authorization_code');
     params.append('code', code);
-    params.append('redirect_uri', isDev ? DEFAULT_REDIRECT_URL : redirectUri);
+    params.append('redirect_uri', isDevBoolean ? DEV_REDIRECT_URL : VITE_REDIRECT_URL);
 
     try {
         const tokenResponse = await axios.post('https://discord.com/api/oauth2/token', params);

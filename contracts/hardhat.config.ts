def9760cbc0b941f4371ac9e7b98ec364ec85c44
Alain Nicolas <alain.nicolas@consensys.net>
2024-10-07 10:52:55 +0200

    feat: Check signatures when issuing an Attestation

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,70 +1,74 @@
-import {HardhatUserConfig} from 'hardhat/config';
+import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import dotenv from 'dotenv';
 
-dotenv.config({path: '../.env'});
+dotenv.config({ path: '../.env' });
 
-const {INFURA_KEY, PRIVATE_KEY, LINEASCAN_API_KEY} = process.env;
+const { INFURA_KEY, PRIVATE_KEY, LINEASCAN_API_KEY } = process.env;
 
 if (
-    INFURA_KEY === undefined ||
-    PRIVATE_KEY === undefined ||
-    LINEASCAN_API_KEY === undefined
+  INFURA_KEY === undefined ||
+  PRIVATE_KEY === undefined ||
+  LINEASCAN_API_KEY === undefined
 ) {
-    throw new Error(
-        'INFURA_KEY, PRIVATE_KEY or LINEASCAN_API_KEY is not set in .env file',
-    );
+  throw new Error(
+    'INFURA_KEY, PRIVATE_KEY or LINEASCAN_API_KEY is not set in .env file',
+  );
 }
 
 const config: HardhatUserConfig = {
-    solidity: {
-        compilers: [
-            {
-                version: '0.8.21',
-                settings: {
-                    evmVersion: 'paris',
-                },
-            },
-        ],
-    },
-    defaultNetwork: 'linea-sepolia',
-    networks: {
-        'linea-sepolia': {
-            url: `https://linea-sepolia.infura.io/v3/${INFURA_KEY}`,
-            accounts: [PRIVATE_KEY],
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.21',
+        settings: {
+          evmVersion: 'london',
+          optimizer: {
+            enabled: true,
+            runs: 2000,
+          },
         },
-        linea: {
-            url: `https://linea-mainnet.infura.io/v3/${INFURA_KEY}`,
-            accounts: [PRIVATE_KEY],
+      },
+    ],
+  },
+  defaultNetwork: 'linea-sepolia',
+  networks: {
+    'linea-sepolia': {
+      url: `https://linea-sepolia.infura.io/v3/${INFURA_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+    linea: {
+      url: `https://linea-mainnet.infura.io/v3/${INFURA_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  paths: {
+    sources: './src',
+  },
+  etherscan: {
+    apiKey: {
+      'linea-sepolia': LINEASCAN_API_KEY,
+      linea: LINEASCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: 'linea-sepolia',
+        chainId: 59141,
+        urls: {
+          apiURL: 'https://api-sepolia.lineascan.build/api',
+          browserURL: 'https://sepolia.lineascan.build',
         },
-    },
-    paths: {
-        sources: './src',
-    },
-    etherscan: {
-        apiKey: {
-            'linea-sepolia': LINEASCAN_API_KEY,
-            linea: LINEASCAN_API_KEY,
+      },
+      {
+        network: 'linea',
+        chainId: 59144,
+        urls: {
+          apiURL: 'https://api.lineascan.build/api',
+          browserURL: 'https://lineascan.build',
         },
-        customChains: [
-            {
-                network: 'linea-sepolia',
-                chainId: 59141,
-                urls: {
-                    apiURL: 'https://api-sepolia.lineascan.build/api',
-                    browserURL: 'https://sepolia.lineascan.build',
-                },
-            },
-            {
-                network: 'linea',
-                chainId: 59144,
-                urls: {
-                    apiURL: 'https://api.lineascan.build/api',
-                    browserURL: 'https://lineascan.build',
-                },
-            },
-        ],
-    },
+      },
+    ],
+  },
 };
 
 export default config;

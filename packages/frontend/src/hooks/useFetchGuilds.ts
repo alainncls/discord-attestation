4ccc60c97f4e04591967297ebcfb97a446d9582c
Alain Nicolas <alain.nicolas.perso@gmail.com>
2024-10-14 23:46:26 +0200

    chore: Clean frontend code and extract hooks

---
+++ packages/frontend/src/hooks/useFetchGuilds.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { DecodedPayload, SignedGuild } from '../types';
+import { Address } from 'viem';
+import { VeraxSdk } from '@verax-attestation-registry/verax-sdk';
+import { PORTAL_ID, SCHEMA_ID } from '../utils/constants';
+
+export const useFetchGuilds = (veraxSdk?: VeraxSdk, address?: Address, code?: string | null) => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [guilds, setGuilds] = useState<SignedGuild[]>([]);
+  const [signedSubject, setSignedSubject] = useState<string>();
+
+  useEffect(() => {
+    if (isLoading && code && veraxSdk) {
+      const fetchGuilds = async () => {
+        localStorage.removeItem('discord_oauth_started');
+        const res = await fetch(`https://discord.alainnicolas.fr/.netlify/functions/api?code=${code}&isDev=${import.meta.env.MODE === 'development'}&subject=${address}`);
+        const data = await res.json();
+        if (!data.error && !data.message) {
+          setIsLoggedIn(true);
+          const attestedGuilds = await veraxSdk.attestation.findBy(1000, 0, {
+            schema: SCHEMA_ID,
+            portal: PORTAL_ID.toLowerCase(),
+            subject: address,
+          });
+          setGuilds(data.signedGuilds.map((guild: SignedGuild) => {
+            const attestedGuild = attestedGuilds.find(attested => (attested.decodedPayload as DecodedPayload[])[0].guildId === guild.id);
+            return attestedGuild ? { ...guild, attestationId: attestedGuild.id } : guild;
+          }));
+          setSignedSubject(data.subjectSignature);
+        }
+        setIsLoading(false);
+      };
+      fetchGuilds();
+    }
+  }, [isLoading, code, address, veraxSdk]);
+
+  return { isLoggedIn, isLoading, guilds, signedSubject, setIsLoading };
+};
---
+++ packages/frontend/src/utils/constants.ts
@@ -0,0 +1,4 @@
+import { Address, Hex } from 'viem';
+
+export const PORTAL_ID: Address = '0x4DA1fD9BF47c73Aa7002F155048a8ba7898C960C';
+export const SCHEMA_ID: Hex = '0xa8d6aefe759739c13a4151523a525bfe88b7dae97bdd5de50dab89cb247690d4';

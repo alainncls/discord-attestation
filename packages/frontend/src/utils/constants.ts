4a5ba5cbca0b586707070f032adddfb29890e0ac
Alain Nicolas <alain.nicolas.perso@gmail.com>
2024-10-21 23:35:14 +0200

    chore(frontend): Target new Portal

--- packages/frontend/src/utils/constants.ts
+++ packages/frontend/src/utils/constants.ts
@@ -1,5 +1,5 @@
 import { Address, Hex } from 'viem';
 
-export const PORTAL_ID: Address = '0x7e2Fa20346eCd4f402Ec9Ebec290e0b2e520DeA5';
+export const PORTAL_ID: Address = '0x743de231B07d34590c3b4511DA102afB8E917594';
 export const SCHEMA_ID: Hex =
   '0xa8d6aefe759739c13a4151523a525bfe88b7dae97bdd5de50dab89cb247690d4';

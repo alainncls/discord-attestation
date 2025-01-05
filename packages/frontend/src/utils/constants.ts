98f1fc94a54e0a179e5b4dfd9eabb743a0c7260f
Alain Nicolas <alain.nicolas@consensys.net>
2025-01-05 01:18:35 +0100

    chore(frontend): Rely on new versions of the Portal and the Schema

--- packages/frontend/src/utils/constants.ts
+++ packages/frontend/src/utils/constants.ts
@@ -1,5 +1,5 @@
 import { Address, Hex } from 'viem';
 
-export const PORTAL_ID: Address = '0x46240436eC7A9cE1e637506a2935BdceB9d8a3aA';
+export const PORTAL_ID: Address = '0x562225b292A775F2cB24f4014Bb8e2cc9316CE93';
 export const SCHEMA_ID: Hex =
-  '0xa8d6aefe759739c13a4151523a525bfe88b7dae97bdd5de50dab89cb247690d4';
+  '0x2dbbaa5d8c394d99470ea8eebe48c52c0042db98d3f16719f9b5717c73487a23';

77d8d29366f39f760f693ccd93c704c844780a34
Alain Nicolas <alain.nicolas@consensys.net>
2026-05-08 19:30:56 +0200

    fix: update hardened portal addresses

--- packages/frontend/src/utils/constants.ts
+++ packages/frontend/src/utils/constants.ts
@@ -1,5 +1,5 @@
 import type { Address, Hex } from 'viem';
 
-export const PORTAL_ID_TESTNET: Address = '0xd0Fb47EA3960d15425137B2fF83955c3beb6a85c';
-export const PORTAL_ID: Address = '0x407e280281B812Adef69A91230659C9D738D82Cb';
+export const PORTAL_ID_TESTNET: Address = '0xf22f348C9257F418509eC03d6BC0C31A20Bd9E46';
+export const PORTAL_ID: Address = '0x6C3Ba43a3c4aC6579D44Baeb84e9CFC23654964f';
 export const SCHEMA_ID: Hex = '0xefa96ce61912c5bb59cb4c26645ea193fc03a234fe09a6b2c8b85aaa51a382d6';
--- packages/functions/lib/constants.ts
+++ packages/functions/lib/constants.ts
@@ -1,5 +1,4 @@
 import type { Address } from 'viem';
 
-export const PORTAL_ID_TESTNET: Address = '0xd0Fb47EA3960d15425137B2fF83955c3beb6a85c';
-export const PORTAL_ID: Address = '0x407e280281B812Adef69A91230659C9D738D82Cb';
-
+export const PORTAL_ID_TESTNET: Address = '0xf22f348C9257F418509eC03d6BC0C31A20Bd9E46';
+export const PORTAL_ID: Address = '0x6C3Ba43a3c4aC6579D44Baeb84e9CFC23654964f';
